0a94732a5dd94a99f2d63bc2ac46ed30b2050ad5
Tomas <tomasgero55@gmail.com>
2024-11-11 10:23:41 -0500

    backend inicio de sesion

--- app.js
+++ app.js
@@ -23,36 +23,63 @@ const connection = mysql.createPool({
 });
 
 app.get('/', (req, res) => {
-  res.send('Manco Decoraciones')
-})
+  res.send('Converso')
+})  
+
 app.get('/login',async (req, res) => {
-    const datos = req.query;
+  const datos = req.query;
 
-    // Una consulta SELECT simple
+  // Una consulta SELECT simple
 try {
-  const [results, fields] = await connection.query(
-  "SELECT * FROM `usuarios` WHERE `usuario` = ? AND `clave` = ?",
-  [datos.usuario, datos.clave]
-  );
-  if (results.length > 0){
-    req.session.usuario = datos.usuario;
-    res.status(200).send('Inicio correctamente')
-  }else{
-    res.status(401).send('Datos incorrectos')
-  }
+const [results, fields] = await connection.query(
+"SELECT * FROM `usuarios` WHERE `usuario` = ? AND `clave` = ?",
+[datos.usuario, datos.clave]
+);
+if (results.length > 0){
+  req.session.usuario = datos.usuario;
+  res.status(200).send('Inicio correctamente')
+}else{
+  res.status(401).send('Datos incorrectos')
+}
 
-  console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra meta data about results, if available
-  } catch (err) {
-  console.log(err);
-  }
+console.log(results); // Los resultados contienen filas devueltas por el servidor
+console.log(fields); // Los campos contienen metadatos adicionales sobre los resultados, si están disponibles
+} catch (err) {
+console.log(err);
+res.status(500).send('Error del servidor')
+}
 })
+
   app.get('/validar', (req, res) => {
     if (req.session.usuario){
       res.status(200).send('Sesion validada')
     }else{
       res.status(401).send('No autorizado')
     }
+})
+
+app.get('/registro',async (req, res) => {
+  const datos = req.query;
+
+  // Una consulta SELECT simple
+try {
+const [results, fields] = await connection.query(
+  "INSERT INTO `usuarios` (`id`, `usuario`, `clave`) VALUES (NULL, ?, ?);",
+  [datos.usuario, datos.clave]
+);
+if (results.affectedRows > 0){
+  req.session.usuario = datos.usuario;
+  res.status(200).send('Inicio correctamente')
+}else{
+  res.status(401).send('Datos incorrectos')
+}
+
+console.log(results); // results contains rows returned by server
+console.log(fields); // fields contains extra meta data about results, if available
+} catch (err) {
+console.log(err);
+res.status(500).send('Error del servidor')
+}
 })
 
 app.listen(port, () => {
